2eea34492b6cb948b70bd40ff0697adbe45770cd
AllanDexter14 <allandextercuizon2006@gmail.com>
2026-08-24 17:12:29 +0800

    add static files and templates

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -8,14 +8,15 @@ class BaseModel(models.Model):
 
 class College(BaseModel):
       college_name = models.CharField(max_length=150)
-      def str(self):
+
+      def __str__(self):
             return self.college_name
 
 class Program(BaseModel):
       prog_name = models.CharField(max_length=150)
       college = models.ForeignKey(College, on_delete=models.CASCADE)
 
-      def str(self):
+      def __str__(self):
            return self.prog_name
 
 class Organization(BaseModel):
@@ -23,7 +24,7 @@ class Organization(BaseModel):
        college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
        description = models.CharField(max_length=500)
 
-       def str(self):
+       def __str__(self):
          return self.name
 
 class Student(BaseModel):
@@ -33,7 +34,7 @@ class Student(BaseModel):
      middlename = models.CharField(max_length=25, blank=True, null=True)
      program = models.ForeignKey(Program, on_delete=models.CASCADE)
 
-     def str(self):
+     def __str__(self):
       return f"{self.lastname}, {self.firstname}"
 
 class OrgMember(BaseModel):
