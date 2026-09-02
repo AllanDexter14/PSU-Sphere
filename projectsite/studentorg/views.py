db53994e37a5945d83b475b0db6d6b9b3ac348d3
AllanDexter14 <allandextercuizon2006@gmail.com>
2026-09-02 12:39:37 +0800

    Add College ListView, CreateView, UpdateView, DeleteView

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from studentorg.models import Organization
-from django.views.generic.edit import CreateView, UpdateView
-from studentorg.forms import OrganizationForm
+from studentorg.models import Organization, College
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from studentorg.forms import OrganizationForm, CollegeForm
 from django.urls import reverse_lazy
 
 
@@ -28,3 +28,32 @@ class OrganizationUpdateView(UpdateView):
     form_class = OrganizationForm
     template_name = 'org_form.html'
     success_url = reverse_lazy('organization-list')
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('organization-list')
+
+class CollegeList(ListView):
+    model = College
+    context_object_name = 'college'
+    template_name = 'college_list.html'
+    paginate_by = 5
+
+class CollegeCreateView(CreateView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college_form.html'
+    success_url = reverse_lazy('college-list')
+
+class CollegeUpdateView(UpdateView):
+    model = College
+    form_class = CollegeForm
+    template_name = 'college_form.html'
+    success_url = reverse_lazy('college-list')
+
+class CollegeDeleteView(DeleteView):
+    model = College
+    template_name = 'college_del.html'
+    success_url = reverse_lazy('college-list')
+
